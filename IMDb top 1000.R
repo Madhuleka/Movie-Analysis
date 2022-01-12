51f9d54f3b467afb74fdf409e34a660af18d0031
Madhuleka <96581557+Madhuleka@users.noreply.github.com>
2022-01-13 00:31:46 +0530

    Removed \n from name and genre

--- IMDb top 1000.R
+++ IMDb top 1000.R
@@ -14,6 +14,7 @@ if (any(installed_packages == FALSE)) {
 
 # Packages loading
 lapply(packages, library, character.only = TRUE)
+
 Movie_Dataframe = data.frame()
 
 for(i in seq(from = 1, to = 1000, by = 250))
@@ -21,10 +22,10 @@ for(i in seq(from = 1, to = 1000, by = 250))
   url <- paste0("https://www.imdb.com/search/title/?groups=top_1000&sort=user_rating,desc&count=250&start=", i, "&ref_=adv_nxt")
   doc <- read_html(url)
 
-  Movie_Dataframe = rbind(Movie_Dataframe, data.frame(Name = doc %>% html_nodes(".lister-item-header a") %>% html_text(),
+  Movie_Dataframe = rbind(Movie_Dataframe, data.frame(Name = doc %>% html_nodes(".lister-item-content h3 a") %>% html_text(),
                                Rating=doc %>% html_nodes(".ratings-imdb-rating strong") %>% html_text(),
                                Runtime=parse_number(doc %>% html_nodes(".runtime") %>% html_text()), 
-                               Genre=doc %>% html_nodes(".genre") %>% html_text(), 
+                               Genre=gsub("[\n]", "", doc %>% html_nodes(".genre") %>% html_text()), 
                                Certificate=c(doc %>% html_nodes(".certificate") %>% html_text(),rep(NA, 250-length(doc %>% html_nodes(".certificate"))))))
 }
 
