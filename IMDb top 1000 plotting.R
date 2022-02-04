f97cd5163051c71e25e36069705c0b9d396bb7d5
Madhuleka <96581557+Madhuleka@users.noreply.github.com>
2022-02-05 02:40:30 +0530

    New Graphs added

--- IMDb top 1000 plotting.R
+++ IMDb top 1000 plotting.R
@@ -16,6 +16,8 @@ lapply(packages, library, character.only = TRUE)
 
 movie_data <- read.csv("movie_data.csv")
 
+#Creates list that initialises variables to count frequency of genre and
+#plots a bar chart to visualise the distribution of genres in the top 1000 list
 
 list_1 = c(0,0,0,0,0,0,0)
 names(list_1) <- c("Genre_1","Genre_2","Genre_3","Genre_4","Genre_5","Genre_6", "Genre_7")
@@ -44,7 +46,11 @@ genre_count<-c(list_1["Genre_1"],list_1["Genre_2"],list_1["Genre_3"],list_1["Gen
 
 barplot(genre_count,names.arg=genre_names,xlab="Genre",ylab="Count",col="blue",
         main="Genre chart")
-pie(genre_count, genre_names, radius = 1, main = "Genre Chart")
+
+
+#Creates list that initialises variables to count frequency of rating and 
+#plot bar chart to visualise the distribution of rating of the movies in the 
+#top 1000 list
 
 list_2 = c(0,0,0,0)
 names(list_2) <- c("Lessthan_8.5","Lessthan_9","Lessthan_9.5","Lessthan_10")
@@ -67,6 +73,15 @@ Rating_name <- c("Less than 8.5", "Between 8.5 and 9", "Between 9 and 9.5", "Gre
 barplot(Rating_list,names.arg=Rating_name,xlab="Rating",ylab="Frequency",col="blue",
         main="Rating chart") 
 
+#Plots a scatterplot runtime vs the rating the movie has received
+
+plot(movie_data$Runtime, list_rating)
+model = lm(list_rating ~ movie_data$Runtime)
+abline(model)
+
+#Creates list that initialises variables to count frequency of decade of release
+#of movies and plot a bar chart to visualise the distribution of the different
+#decades in the top 1000 list
 
 list_3 = c(0,0,0,0,0,0,0)
 names(list_3) <- c("60s_and_before", "70s", "80s","90s","00s","10s","20s")
@@ -93,6 +108,10 @@ Year_list <- c("<70s", "70s", "80s", "90s", "00s", "10s", "20s")
 Year_count <- c(list_3["60s_and_before"], list_3["70s"], list_3["80s"], list_3["90s"], list_3["00s"], list_3["10s"], list_3["20s"])
 barplot(Year_count, names.arg = Year_list, xlab = "Year", ylab = "Frequency", col = "blue", main = "Year chart")
 
+#Creates list that initialises variables to count frequency of number of votes
+#and plots a bar chart to visualise the distribution of the number of votes
+#the movies on the top 1000 list
+
 list_4 = c(0,0,0,0,0,0)
 names(list_4) = c("<50k", "<100k", "<250k", "<500k", "<750k", "<1m")
 for(element in movie_data$Number_of_Votes)
@@ -112,8 +131,12 @@ for(element in movie_data$Number_of_Votes)
 }
 
 Vote_list <- c("<50k", "<100k", "<250k", "<500k", "<750k", "<1m")
-Vote_Count <- c(list_4["<50k"], list_4["<100k"], list_4["<250k"], list_4["<500k"], list_4["<750k"], list_4["<1m"])
+Vote_Count <- c(list_4["<50k"], list_4["<100k"], list_4["<250k"], list_4["<500k"], list_4["<750k"], list_4[">1m"])
 barplot(Vote_Count, name.arg = Vote_list, xlab = "Vote Count", ylab = "Frequency", col = "blue", main = "Votes Chart")
+
+
+#Creates a matrix with rating as rows and the number of votes as columns and
+#plots a line graph to visualise the trend among different rating
 
 list_votes = c()
 for(element in movie_data$Number_of_Votes)
@@ -169,6 +192,168 @@ for(i in 1:1000)
   }
 
 
-heatmap(t(rating_count))
+#Plots scatter plot of rating vs the log of the number of votes
 
 
+plot(log(list_votes), list_rating)
+model = lm(list_rating ~ log(list_votes))
+abline(model)
+
+col_set <- rainbow(5)
+par(mar=c(5.1, 4.1, 4.1, 8), xpd=TRUE)
+matplot(t(rating_count), type = "l", xlab="Rating",col=col_set,lty=c(1:14),lwd=3)
+xcolnames<-c("<50k","<100k", "<250k", "<500k", "<750k", ">750k")
+names <- c("8","8.5", "9", "9.5", "10")
+axis(side=1, at=1:length(colnames), labels=colnames)
+legend("right", inset = c(-0.35,0 ),                   
+       legend = names,xpd=TRUE,cex=0.8, lty=c(1:14),col=col_set,lwd=3)
+
+
+
+
+#Creates a matrix with genres as rows and the number of votes as columns and
+#plots a line graph to visualise the trend among different genres
+
+list_genre = c()
+
+for(element in movie_data$Genre)
+{
+  y = unlist(strsplit(element,","))
+  list_genre = append(list_genre, element)
+}
+
+
+genre_vs_votes=matrix(0,14,6)
+rownames(genre_vs_votes)=c("Adventure", "War", "Action", "Drama", "Family", 
+                           "Fantasy", "Sci-Fi", "Crime", "Mystery", "Thriller", 
+                           "Biography", "Comedy", "Romance", "Horror")
+colnames(genre_vs_votes)=c("<50k","<100k", "<250k", "<500k", "<750k", ">750")
+
+
+
+
+for(i in 1:1000)
+{ 
+  
+  j = case_when(
+    grepl("Adventure", list_genre[i], fixed=TRUE,ignore.case = FALSE) ~ 1,
+    grepl("War", list_genre[i], fixed=TRUE,ignore.case = FALSE) ~ 2,
+    grepl("Action", list_genre[i], fixed=TRUE,ignore.case = FALSE) ~ 3,
+    grepl("Drama", list_genre[i], fixed=TRUE,ignore.case = FALSE) ~ 4,
+    grepl("Family", list_genre[i], fixed=TRUE,ignore.case = FALSE) ~ 5,
+    grepl("Fantasy", list_genre[i], fixed=TRUE,ignore.case = FALSE) ~ 6,
+    grepl("Sci-Fi", list_genre[i], fixed=TRUE,ignore.case = FALSE) ~ 7,
+    grepl("Crime", list_genre[i], fixed=TRUE,ignore.case = FALSE) ~ 8,
+    grepl("Mystery", list_genre[i], fixed=TRUE,ignore.case = FALSE) ~ 9,
+    grepl("Thriller", list_genre[i], fixed=TRUE,ignore.case = FALSE) ~ 10,
+    grepl("Biography", list_genre[i], fixed=TRUE,ignore.case = FALSE) ~ 11,
+    grepl("Comedy", list_genre[i], fixed=TRUE,ignore.case = FALSE) ~ 12,
+    grepl("Romance", list_genre[i], fixed=TRUE,ignore.case = FALSE) ~ 13,
+    grepl("Horror", list_genre[i], fixed=TRUE,ignore.case = FALSE) ~ 14
+  )
+
+  if(list_votes[i]<= 50000)
+  {
+    genre_vs_votes[j,1] <- genre_vs_votes[j,1] + 1
+  }
+  if(list_votes[i] > 50000 && list_votes[i]<= 100000)
+  {
+    genre_vs_votes[j,2] <- genre_vs_votes[j,2] + 1
+  }
+  if(list_votes[i] > 100000 && list_votes[i]<= 250000)
+  {
+    genre_vs_votes[j,3] <- genre_vs_votes[j,3] + 1
+  }
+  if(list_votes[i] > 250000 && list_votes[i]<= 500000)
+  {
+    genre_vs_votes[j,4] <- genre_vs_votes[j,4] + 1
+  }
+  if(list_votes[i] > 500000 && list_votes[i]<= 750000)
+  {
+    genre_vs_votes[j,5] <- genre_vs_votes[j,5] + 1
+  }
+  if(list_votes[i] > 750000)
+  {
+    genre_vs_votes[j,6] <- genre_vs_votes[j,6] + 1
+  }
+}
+
+#Plots line graph depicting the trend in number of votes among 
+#different genres
+col_set <- rainbow(14)
+colnames<-c("<50k","<100k", "<250k", "<500k", "<750k", ">750")
+genres = c("Adventure", "War", "Action", "Drama", "Family", 
+           "Fantasy", "Sci-Fi", "Crime", "Mystery", "Thriller", 
+           "Biography", "Comedy", "Romance", "Horror")
+
+par(mar=c(5.1, 4.1, 4.1, 8), xpd=TRUE)
+matplot(t(genre_vs_votes), type = "l", xlab="Number of Votes",col=col_set,lty=c(1:14),lwd=3)
+axis(side=1, at=1:length(colnames), labels=colnames)
+legend("right", inset = c(-0.35,0 ),                   
+       legend = genres,xpd=TRUE,cex=0.8, lty=c(1:14),col=col_set,lwd=3)
+
+
+#Creates a matrix with genres as rows and the rating as columns and
+#plots a line graph to visualise the trend among different genres
+
+genre_vs_rating=matrix(0, 14, 5)
+rownames(genre_vs_rating)=c("Adventure", "War", "Action", "Drama", "Family", 
+                           "Fantasy", "Sci-Fi", "Crime", "Mystery", "Thriller", 
+                           "Biography", "Comedy", "Romance", "Horror")
+colnames(genre_vs_rating)=c("8","8.5", "9", "9.5", "10")
+
+for(i in 1:1000)
+{ 
+  
+  j = case_when(
+    grepl("Adventure", list_genre[i], fixed=TRUE,ignore.case = FALSE) ~ 1,
+    grepl("War", list_genre[i], fixed=TRUE,ignore.case = FALSE) ~ 2,
+    grepl("Action", list_genre[i], fixed=TRUE,ignore.case = FALSE) ~ 3,
+    grepl("Drama", list_genre[i], fixed=TRUE,ignore.case = FALSE) ~ 4,
+    grepl("Family", list_genre[i], fixed=TRUE,ignore.case = FALSE) ~ 5,
+    grepl("Fantasy", list_genre[i], fixed=TRUE,ignore.case = FALSE) ~ 6,
+    grepl("Sci-Fi", list_genre[i], fixed=TRUE,ignore.case = FALSE) ~ 7,
+    grepl("Crime", list_genre[i], fixed=TRUE,ignore.case = FALSE) ~ 8,
+    grepl("Mystery", list_genre[i], fixed=TRUE,ignore.case = FALSE) ~ 9,
+    grepl("Thriller", list_genre[i], fixed=TRUE,ignore.case = FALSE) ~ 10,
+    grepl("Biography", list_genre[i], fixed=TRUE,ignore.case = FALSE) ~ 11,
+    grepl("Comedy", list_genre[i], fixed=TRUE,ignore.case = FALSE) ~ 12,
+    grepl("Romance", list_genre[i], fixed=TRUE,ignore.case = FALSE) ~ 13,
+    grepl("Horror", list_genre[i], fixed=TRUE,ignore.case = FALSE) ~ 14
+  )
+
+if(list_rating[i]<= 8)
+{
+  genre_vs_rating[j,1] <- genre_vs_rating[j,1] + 1
+}
+if(list_rating[i] > 8 && list_rating[i]<= 8.5)
+{
+  genre_vs_rating[j,2] <- genre_vs_rating[j,2] + 1
+}
+if(list_rating[i] > 8.5 && list_rating[i]<= 9)
+{
+  genre_vs_rating[j,3] <- genre_vs_rating[j,3] + 1
+}
+if(list_rating[i] > 9 && list_rating[i]<= 9.5)
+{
+  genre_vs_rating[j,4] <- genre_vs_rating[j,4] + 1
+}
+if(list_rating[i] > 9.5 && list_rating[i]<= 10)
+{
+  genre_vs_rating[j,5] <- genre_vs_rating[j,5] + 1
+}
+}
+
+
+
+#Plots line graph depicting the trend in rating among 
+#different genres
+
+col_set <- rainbow(14)
+colnames<-c("8","8.5", "9", "9.5", "10")
+
+par(mar=c(5.1, 4.1, 4.1, 8), xpd=TRUE)
+matplot(t(genre_vs_rating), type = "l", xlab="Rating",col=col_set,lty=c(1:14),lwd=3)
+axis(side=1, at=1:length(colnames), labels=colnames)
+legend("right", inset = c(-0.35,0 ),                   
+       legend = genres,xpd=TRUE,cex=0.8, lty=c(1:14),col=col_set,lwd=3)
